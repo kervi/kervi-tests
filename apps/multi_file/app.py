6d90b91e7fc135061ff03b66d17a48b49ee68c30
Tim Wentzlau <tim@elektron.fo>
2017-03-09 23:54:00 +0000

    Added module tests

---
+++ apps/multi_file/app.py
@@ -0,0 +1,22 @@
+""" My kervi application """
+from kervi.bootstrap import Application
+import kervi.utility.nethelper as nethelper
+
+if __name__ == '__main__':
+    APP = Application({
+        "info":{
+            "id":"app",
+            "name":"Test multi file app",
+            "appKey":"",
+        },
+        "modules":["sensors", "controllers", "cams"],
+        "network":{
+            "IPAddress": nethelper.get_ip_address(),
+            "IPCRootPort":9500,
+            "WebSocketPort":9000,
+            "WebPort": 8080,
+            "IPCSecret":b"fd9969b3-9748-46b6-a69d-119ec2773352"
+        },
+    })
+
+    APP.run()
--- modules/module.py
+++ modules/module.py
@@ -18,7 +18,19 @@ if __name__ == '__main__':
     })
 
     from kervi.dashboard import Dashboard, DashboardPanel
-    DASHBOARD = Dashboard("module-dashboard", "module dashboard", is_default=True)
+    DASHBOARD = Dashboard("dashboard.module", "module dashboard", is_default=True)
     DASHBOARD.add_panel(DashboardPanel("panel", columns=2, rows=2, title="Panel dashboard"))
+
+
+    from kervi.sensor import Sensor
+    from kervi_devices.platforms.common.sensors.memory_use import MemUseSensorDeviceDriver
+    SENSOR_1 = Sensor("MemLoadSensor", "MEM", MemUseSensorDeviceDriver())
+    #link to sys area top right
+    SENSOR_1.link_to_dashboard("*", "sys-header")
+    #link to a panel, show value in panel header and chart in panel body
+    SENSOR_1.link_to_dashboard("dashboard.module", "panel", type="value", size=2, link_to_header=True)
+    SENSOR_1.link_to_dashboard("dashboard.module", "panel", type="chart", size=2)
+
+
 
     APP_MODULE.run()
