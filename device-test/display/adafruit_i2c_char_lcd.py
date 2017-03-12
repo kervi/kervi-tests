b85f7a567d3b96cca510bda7dd36893a56376078
Tim Wentzlau <tim@elektron.fo>
2017-03-12 15:04:13 +0000

    Add adafruit i2c char lcd test

---
+++ device-test/display/adafruit_i2c_char_lcd.py
@@ -0,0 +1,32 @@
+if __name__ == '__main__':
+    from kervi.bootstrap import Application
+    APP = Application()
+
+    #add dashboard and panel
+    from kervi.dashboard import Dashboard, DashboardPanel
+    DASHBOARD = Dashboard("dashboard.ctrl", "Adafruit char lcd test", is_default=True)
+    DASHBOARD.add_panel(DashboardPanel("input", columns=2, rows=4, title="input"))
+    DASHBOARD.add_panel(DashboardPanel("log", columns=3, rows=4, title="Log", user_log=True))
+
+    #define a light controller
+    from kervi.hal import GPIO
+    from kervi.controller import Controller, UITextControllerInput
+    from kervi_devices.displays.adafruit_char_lcd import I2CAdafruitCharLCDPlate
+
+    class TestController(Controller):
+        def __init__(self):
+            Controller.__init__(self, "controller.test", "test")
+            self.type = "test"
+
+            self.lcd = I2CAdafruitCharLCDPlate(0x48, 1)
+            print("gpio:", self.lcd.device_name)
+            self.text = UITextControllerInput("lcd.text", "LCD text", self)
+            self.text.link_to_dashboard("dashboard.ctrl", "input")
+
+        def input_changed(self, changed_input):
+            self.user_log_message("input changed:{0} value:{1}".format(changed_input.input_id, changed_input.value))
+            self.lcd.message(changed_input.value)
+
+    TestController()
+
+    APP.run()
