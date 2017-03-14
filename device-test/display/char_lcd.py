2c81f077bc445b0b9b9b09ec2854469195ba0473
pi <pi@wentzlau.dk>
2017-03-14 22:01:29 +0000

    update lcd test

--- device-test/display/char_lcd.py
+++ device-test/display/char_lcd.py
@@ -11,21 +11,18 @@ if __name__ == '__main__':
     #define a light controller
     from kervi.hal import GPIO
     from kervi.controller import Controller, UITextControllerInput
-    from kervi_devices.displays.char_lcd import PCFCharLCD
+    from kervi_devices.displays.HD44780_i2c_PCF8574 import CharLCDDeviceDriver
 
     class TestController(Controller):
         def __init__(self):
             Controller.__init__(self, "controller.test", "test")
             self.type = "test"
 
-<<<<<<< HEAD:device-test/display/adafruit_i2c_char_lcd.py
-            self.lcd = I2CAdafruitCharLCDPlate(0x3f, 1)
-            self.lcd.message("LCD test ready") 
-            # print("gpio:", self.lcd.device_name)
-=======
-            self.lcd = PCFCharLCD()
+            self.lcd = CharLCDDeviceDriver()
+            self.lcd.enable_display(True)
+            self.lcd.message("test")
+            self.lcd.set_backlight(True)
             #print("gpio:", self.lcd.device_name)
->>>>>>> 25cbf07d3ddc0ac436b00c702e4e81ddce13bc73:device-test/display/char_lcd.py
             self.text = UITextControllerInput("lcd.text", "LCD text", self)
             self.text.link_to_dashboard("dashboard.ctrl", "input")
 
@@ -33,6 +30,11 @@ if __name__ == '__main__':
             self.user_log_message("input changed:{0} value:{1}".format(changed_input.input_id, changed_input.value))
             self.lcd.message(changed_input.value)
 
+        def exit(self):
+            self.lcd.message("")
+            self.lcd.set_backlight(False)
+            self.lcd.enable_display(False)
+            
     TestController()
 
     APP.run()
