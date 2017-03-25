51a3519b2c2fcd3c19d15cbfbff323016ea81fd6
Tim Wentzlau <tim@elektron.fo>
2017-03-25 14:17:50 +0000

    update adafruit motor hat test

--- device-test/motors/adafruit_motor_hat.py
+++ device-test/motors/adafruit_motor_hat.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     #define a light controller
     #from kervi.hal import GPIO
-    from kervi.controller import Controller, UINumberControllerInput, UISwitchButtonControllerInput
+    from kervi.controller import Controller, UINumberControllerInput, UIButtonControllerInput, UISwitchButtonControllerInput
     from kervi_devices.motors.adafruit_i2c_motor_hat import AdafruitMotorHAT
 
     class TestController(Controller):
@@ -21,7 +21,7 @@ if __name__ == '__main__':
             self.motor_controller = AdafruitMotorHAT()
             self.dc_motor = self.motor_controller.dc_motors[0]
             self.stepper_motor = self.motor_controller.stepper_motors[1]
-            
+
             #print("motor driver:", self.motor_controller.device_name)
 
             self.speed = UINumberControllerInput("speed", "dc speed", self)
@@ -30,6 +30,9 @@ if __name__ == '__main__':
             self.step_interval = UINumberControllerInput("step_interval", "step_interval", self)
             self.step_interval.link_to_dashboard("dashboard.ctrl", "input")
 
+            self._step = UIButtonControllerInput("step", "Step", self)
+            self._step.link_to_dashboard("dashboard.ctrl", "input")
+
             self._start_stepper = UISwitchButtonControllerInput("start_stepper", "Start step", self)
             self._start_stepper.link_to_dashboard("dashboard.ctrl", "input")
 
@@ -37,8 +40,10 @@ if __name__ == '__main__':
             self.user_log_message("input changed:{0} value:{1}".format(changed_input.input_id, changed_input.value))
             if changed_input == self.speed:
                 self.dc_motor.run(changed_input.value)
-            if changed_input == self.speed2:
-                self.motor2.speed(changed_input.value)
+            if changed_input == self.step_interval:
+                self.stepper_motor.step_interval = changed_input.value
+            if changed_input == self._step:
+                self.stepper_motor.step()
     TestController()
 
     APP.run()
