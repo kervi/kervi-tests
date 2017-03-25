b63863b886d59588a4f169035e6dd60b02c24b87
Tim Wentzlau <tim@elektron.fo>
2017-03-25 17:16:04 +0000

    update test

--- device-test/motors/adafruit_motor_hat.py
+++ device-test/motors/adafruit_motor_hat.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     #define a light controller
     #from kervi.hal import GPIO
-    from kervi.controller import Controller, UINumberControllerInput, UIButtonControllerInput, UISwitchButtonControllerInput
+    from kervi.controller import Controller, UINumberControllerInput, UISelectControllerInput, UIButtonControllerInput, UISwitchButtonControllerInput
     from kervi_devices.motors.adafruit_i2c_motor_hat import AdafruitMotorHAT
 
     class TestController(Controller):
@@ -27,24 +27,42 @@ if __name__ == '__main__':
             self.speed = UINumberControllerInput("speed", "dc speed", self)
             self.speed.link_to_dashboard("dashboard.ctrl", "input")
 
-            self.step_interval = UINumberControllerInput("step_interval", "step_interval", self)
+
+            self.steps = UINumberControllerInput("steps", "Steps", self)
+            self.steps.link_to_dashboard("dashboard.ctrl", "input")
+
+            self.step_interval = UINumberControllerInput("step_interval", "Step interval", self)
+            self.step_interval.link_to_dashboard("dashboard.ctrl", "input")
+
+            self.step_type = UISelectControllerInput("step_type", "step type", self)
+            self.step_type.add_option(1, "SINGLE")
+            self.step_type.add_option(2, "DOUBLE")
+            self.step_type.add_option(3, "Interleave")
+            self.step_type.add_option(4, "MICROSTEP")
+
             self.step_interval.link_to_dashboard("dashboard.ctrl", "input")
 
             self._step = UIButtonControllerInput("step", "Step", self)
             self._step.link_to_dashboard("dashboard.ctrl", "input")
 
-            self._start_stepper = UISwitchButtonControllerInput("start_stepper", "Start step", self)
-            self._start_stepper.link_to_dashboard("dashboard.ctrl", "input")
+            self._stepper_dir = UISwitchButtonControllerInput("start_stepper", "Direction", self)
+            self._stepper_dir.link_to_dashboard("dashboard.ctrl", "input")
 
         def input_changed(self, changed_input):
             self.user_log_message("input changed:{0} value:{1}".format(changed_input.input_id, changed_input.value))
             if changed_input == self.speed:
-                self.dc_motor.run(changed_input.value)
+                self.dc_motor.set_speed(changed_input.value)
+
             if changed_input == self.step_interval:
                 self.stepper_motor.step_interval = changed_input.value
+
+            if changed_input == self.step_type:
+                self.stepper_motor.step_type = changed_input.value
+
             if changed_input == self._step:
-                self.stepper_motor.step(10)
-                self.stepper_motor.step(-10)
+                if changed_input.value:
+                    self.stepper_motor.step(self.steps.value)
+                    self.stepper_motor.release()
     TestController()
 
     APP.run()
