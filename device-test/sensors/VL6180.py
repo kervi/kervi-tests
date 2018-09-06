ed65d5ff395a322ef56918ada0c465b19ed8b5eb
Tim Wentzlau <tim@wentzlau.dk>
2018-09-06 21:52:18 +0100

    update api changes

--- device-test/sensors/VL6180.py
+++ device-test/sensors/VL6180.py
@@ -5,12 +5,11 @@ if __name__ == '__main__':
     #add dashboard and panel
     
     from kervi.sensors.sensor import Sensor
-    from kervi.devices.sensors.VL6180X import VL6180DistanceDeviceDriver, VL6180LuxDeviceDriver
-    sensor = Sensor("vl6180x_distnace", "Distance", VL6180DistanceDeviceDriver())
+    from kervi.devices.sensors.VL6180X import VL6180XDistanceDeviceDriver, VL6180XLuxDeviceDriver
+    sensor = Sensor("vl6180x_distnace", "Distance", VL6180XDistanceDeviceDriver())
     sensor.link_to_dashboard()
 
-    sensor1 = Sensor("vl6180x_lux", "lux", VL6180LuxDeviceDriver())
+    sensor1 = Sensor("vl6180x_lux", "lux", VL6180XLuxDeviceDriver())
     sensor1.link_to_dashboard()
-
 
     APP.run()
