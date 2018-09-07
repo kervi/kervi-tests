f44154ad3728de8aeee8f355640e44cf7257db00
Tim Wentzlau <tim@wentzlau.dk>
2018-09-07 22:59:59 +0100

    Add test for device

---
+++ device-test/sensors/TCS34725.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    from kervi.application import Application
+    APP = Application()
+
+    
+    from kervi.hal import GPIO
+    from kervi.sensors.sensor import Sensor
+    from kervi.devices.sensors.TCS34725 import TCS34725DeviceDriver
+
+    SENSOR = Sensor("color", "Color", TCS34725DeviceDriver())
+    SENSOR.link_to_dashboard()
+    
+    APP.run()
