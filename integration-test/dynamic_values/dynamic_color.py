eb909939df60662c4716713cf335f8ddb287a96e
Tim Wentzlau <tim@wentzlau.dk>
2018-09-11 08:53:11 +0100

    update sensor tests

---
+++ integration-test/dynamic_values/dynamic_color.py
@@ -0,0 +1,66 @@
+if __name__ == '__main__':
+    from kervi.application import Application
+    APP = Application()
+
+    #add dashboard and panel
+    from kervi.dashboards import Dashboard, DashboardPanel
+    DASHBOARD = Dashboard("dashboard", "colortest", is_default=True)
+    DASHBOARD.add_panel(DashboardPanel("color", width=25, title="Color"))
+    DASHBOARD.add_panel(DashboardPanel("color_inline", title="Color inline"))
+    DASHBOARD.add_panel(DashboardPanel("log", title="Log", user_log=True))
+
+    from kervi.controllers.controller import Controller
+    from kervi.values import ColorValue
+    from kervi.messaging import Messaging
+
+    class TestController(Controller):
+        def __init__(self):
+            Controller.__init__(self, "controller.test", "test")
+            self.type = "test"
+
+            #define an input and link it to the dashboard panel
+            self.color = self.inputs.add("color", "Boolean", ColorValue)
+            self.color.value = "#000f00"
+            self.color.link_to_dashboard("dashboard", "boolean")
+            self.color.link_to_dashboard("dashboard", "boolean", label="#", label_icon="lightbulb-o")
+            self.color.link_to_dashboard(
+                "dashboard",
+                "boolean",
+                label="#",
+            )
+
+            self.inputs["color"].link_to_dashboard(
+                "dashboard",
+                "color",
+                label="#",
+                
+                
+            )
+
+
+            
+
+            self.inputs["color"].link_to_dashboard(
+                "color",
+                "color_inline",
+                label="#inline",
+                inline=True
+                
+            )
+
+            self.color_out = self.outputs.add("color_out", "Color out", ColorValue)   
+            self.color_out.link_to_dashboard()
+
+        def input_changed(self, changed_input):
+            Messaging.send_message("input changed:{0} value:{1}".format(changed_input.value_id, changed_input.value))
+            self.color_out.value = changed_input.value
+
+    TestController()
+
+    from kervi.sensors.sensor import Sensor
+    from kervi.devices.sensors.dummy_sensor import DummyColorSensorDeviceDriver
+    from kervi.devices.sensors.TCS34725 import TCS34725DeviceDriver
+    sensor = Sensor("color_sensor", "Color sensor", TCS34725DeviceDriver())
+    sensor.link_to_dashboard()
+
+    APP.run()
--- integration-test/sensor/sensors.py
+++ integration-test/sensor/sensors.py
@@ -3,43 +3,49 @@
 
 if __name__ == '__main__':
     from kervi.application import Application
-    from kervi import tasks
-    APP = Application()
+    APP = Application({
+        "network":{
+            "ip": "127.0.0.1"
+        }
+    })
 
     #add dashboard and panel
-    from kervi.dashboard import Dashboard, DashboardPanel
+    from kervi.dashboards import Dashboard, DashboardPanel
     DASHBOARD = Dashboard("system", "Sensor test", is_default=True)
-    DASHBOARD.add_panel(DashboardPanel("multi_sensor", columns=3, rows=4, title="Multi sensor"))
-    DASHBOARD.add_panel(DashboardPanel("multi_sensor_single", columns=3, rows=4, title="Multi sensor single"))
-    DASHBOARD.add_panel(DashboardPanel("gauge", columns=3, rows=4, title="Gauge"))
-    DASHBOARD.add_panel(DashboardPanel("log", columns=3, rows=4, title="Log", user_log=True))
+    DASHBOARD.add_panel(DashboardPanel("multi_sensor", title="Multi sensor"))
+    DASHBOARD.add_panel(DashboardPanel("multi_sensor_single", title="Multi sensor single"))
+    DASHBOARD.add_panel(DashboardPanel("gauge", title="Gauge"))
+    DASHBOARD.add_panel(DashboardPanel("log", title="Log", user_log=True))
 
-    from kervi.sensor import Sensor
-    from kervi_devices.platforms.common.sensors.cpu_use import CPULoadSensorDeviceDriver
-    from kervi_devices.sensors.dummy_sensor import DummyMultiDimSensorDeviceDriver
+    from kervi.sensors.sensor import Sensor
+    from kervi.devices.platforms.common.sensors.cpu_use import CPULoadSensorDeviceDriver
+    from kervi.devices.sensors.dummy_sensor import DummyMultiDimSensorDeviceDriver
 
     cpu_sensor = Sensor("CPULoadSensor","CPU", CPULoadSensorDeviceDriver())
     cpu_sensor.store_to_db = False
-    cpu_sensor.link_to_dashboard("*", "sys-header")
-    cpu_sensor.link_to_dashboard("system", "cpu", type="value", size=2, link_to_header=True)
-    cpu_sensor.link_to_dashboard("system", "cpu", type="chart", size=2)
+    cpu_sensor.link_to_dashboard("*", "header_right", show_sparkline=True)
+    cpu_sensor.link_to_dashboard("system", "cpu", type="value", link_to_header=True)
+    cpu_sensor.link_to_dashboard("system", "cpu", type="chart")
 
 
     multi_sensor = Sensor("MultiSensor", "multi sensor", DummyMultiDimSensorDeviceDriver())
     multi_sensor.store_to_db = False
-    multi_sensor.link_to_dashboard("system", "multi_sensor", type="value", size=1)
+    multi_sensor.link_to_dashboard("system", "multi_sensor", type="value")
 
-    multi_sensor[0].link_to_dashboard("system", "multi_sensor_single", type="value", size=2)
-    multi_sensor[1].link_to_dashboard("system", "multi_sensor_single", type="value", size=2)
-    multi_sensor[2].link_to_dashboard("system", "multi_sensor_single", type="value", size=2)
+    multi_sensor[0].link_to_dashboard("system", "multi_sensor_single", type="value")
+    multi_sensor[1].link_to_dashboard("system", "multi_sensor_single", type="value")
+    multi_sensor[2].link_to_dashboard("system", "multi_sensor_single", type="value")
 
-    multi_sensor[0].link_to_dashboard("system", "gauge", type="horizontal_linear_gauge", size=2)
-    multi_sensor[1].link_to_dashboard("system", "gauge", type="vertical_linear_gauge", size=2)
+    multi_sensor[0].link_to_dashboard("system", "gauge", type="horizontal_linear_gauge")
+    multi_sensor[1].link_to_dashboard("system", "gauge", type="vertical_linear_gauge")
 
     multi_sensor[2].add_error_range((0, 10), "l error message")
     multi_sensor[2].add_warning_range((10, 20), "l warning message")
     multi_sensor[2].add_warning_range((80, 90), "h warning message")
     multi_sensor[2].add_error_range((90, 100), "h error message")
-    multi_sensor[2].link_to_dashboard("system", "gauge", type="radial_gauge", size=1)
+    #multi_sensor[2].add_normal_range((20, 80), "normal message")
+    multi_sensor[2].link_to_dashboard("system", "gauge", type="radial_gauge")
+
+    multi_sensor.enable.link_to_dashboard()
 
     APP.run()
