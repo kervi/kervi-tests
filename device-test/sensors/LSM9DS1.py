c46e5986afe984e7a839e2ff1bc13596a7995e5c
Tim Wentzlau <tim@wentzlau.dk>
2019-04-19 17:49:24 +0100

    Update device test

--- device-test/sensors/LSM9DS1.py
+++ device-test/sensors/LSM9DS1.py
@@ -4,8 +4,10 @@ if __name__ == '__main__':
 
     #add dashboard and panel
     from kervi.dashboards import Dashboard, DashboardPanel
-    DASHBOARD = Dashboard("dashboard.app", "Test BMP085", is_default=True)
-    DASHBOARD.add_panel(DashboardPanel("sensor", columns=2, rows=4, title=None))
+    DASHBOARD = Dashboard("dashboard.app", "Test LSM9DS1.py", is_default=True)
+    DASHBOARD.add_panel(DashboardPanel("accl", title="Acceleration"))
+    DASHBOARD.add_panel(DashboardPanel("gyro", title="Gyro"))
+    DASHBOARD.add_panel(DashboardPanel("magnetic", title="Magnetic"))
 
     from kervi.hal import GPIO
     from kervi.sensors import Sensor
@@ -13,12 +15,12 @@ if __name__ == '__main__':
     import kervi.devices.sensors.LSM9DS1 as LSM9DS1
 
     SENSOR_ACCL = Sensor("accl", "Acceleration", LSM9DS1.LSM9DS1AccelerationDeviceDriver())
-    SENSOR_ACCL.link_to_dashboard("dashboard.app", "sensor")
+    SENSOR_ACCL.link_to_dashboard("dashboard.app", "accl")
 
-    SENSOR_GYRO = Sensor("gyro", "gyro", LSM9DS1.LSM9DS1GyroDeviceDriver)
-    SENSOR_GYRO.link_to_dashboard("dashboard.app", "sensor")
+    SENSOR_GYRO = Sensor("gyro", "gyro", LSM9DS1.LSM9DS1GyroDeviceDriver())
+    SENSOR_GYRO.link_to_dashboard("dashboard.app", "gyro")
 
-    SENSOR_MAG = Sensor("magnetic", "Magnetic", LSM9DS1.LSM9DS1MagneticDeviceDriver)
-    SENSOR_MAG.link_to_dashboard("dashboard.app", "sensor")
+    SENSOR_MAG = Sensor("magnetic", "Magnetic", LSM9DS1.LSM9DS1MagneticDeviceDriver())
+    SENSOR_MAG.link_to_dashboard("dashboard.app", "magnetic")
 
     APP.run()
